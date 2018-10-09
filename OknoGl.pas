3f7dc26c60e2a4842de040c9ff9e82c5720c15b7
Piotr Daszewski <43205084+pdaszewski@users.noreply.github.com>
2018-10-09 06:20:26 +0200

    Add files via upload
    uploading version 2.0.2

--- OknoGl.pas
+++ OknoGl.pas
@@ -5,7 +5,7 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, ComCtrls, ExtCtrls, jpeg, StdCtrls, Buttons, math, Mask, Spin,
-  DBCtrls, DB, Grids, DBGrids, ADODB, TeEngine, Series, TeeProcs, Chart,ShellAPI,
+  DBCtrls, DB, Grids, DBGrids, ADODB, TeEngine, Series, TeeProcs, Chart, ShellAPI,
   VclTee.TeeGDIPlus, Vcl.Imaging.pngimage;
 
 type
@@ -32,7 +32,7 @@ type
     Label9: TLabel;
     Label10: TLabel;
     Label11: TLabel;
-    CheckBox1: TCheckBox;
+    cbox_algorythm: TCheckBox;
     ADOConnection1: TADOConnection;
     TabSheet2: TTabSheet;
     ADODataSet1: TADODataSet;
@@ -42,7 +42,7 @@ type
     DBNavigator1: TDBNavigator;
     Label12: TLabel;
     DBLookupComboBox1: TDBLookupComboBox;
-    SpeedButton2: TSpeedButton;
+    btn_change_isotope: TSpeedButton;
     TabSheet3: TTabSheet;
     Chart1: TChart;
     Series1: TAreaSeries;
@@ -51,7 +51,7 @@ type
     Edit5: TEdit;
     SpinEdit2: TSpinEdit;
     Label15: TLabel;
-    SpeedButton3: TSpeedButton;
+    btn_create_chart: TSpeedButton;
     Label16: TLabel;
     SpeedButton4: TSpeedButton;
     TabSheet4: TTabSheet;
@@ -65,8 +65,8 @@ type
     Label23: TLabel;
     procedure SpeedButton1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure SpeedButton2Click(Sender: TObject);
-    procedure SpeedButton3Click(Sender: TObject);
+    procedure btn_change_isotopeClick(Sender: TObject);
+    procedure btn_create_chartClick(Sender: TObject);
     procedure SpeedButton4Click(Sender: TObject);
     procedure Label23Click(Sender: TObject);
     procedure ShellOpen(const Url: string; const Params: string = '');
@@ -85,118 +85,125 @@ implementation
 
 procedure TAOknoGl.FormCreate(Sender: TObject);
 begin
-ADOConnection1.Close;
-ADOConnection1.ConnectionString:='Provider=Microsoft.Jet.OLEDB.4.0;Data Source='+ExtractFilePath(Application.ExeName)+'BazaIzotopow.mdb;Persist Security Info=False;';
-ADOConnection1.Connected:=True;
-ADODataSet1.Open;
-TabSheet3.TabVisible:=False;
+  ADOConnection1.Close;
+  ADOConnection1.ConnectionString := 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=' +
+    ExtractFilePath(Application.ExeName) + 'BazaIzotopow.mdb;Persist Security Info=False;';
+  ADOConnection1.Connected := True;
+  ADODataSet1.Open;
+  TabSheet3.TabVisible := False;
 end;
 
 procedure TAOknoGl.Label23Click(Sender: TObject);
 begin
-ShellOpen(Label23.Caption);
+  ShellOpen(Label23.Caption);
 end;
 
 procedure TAOknoGl.SpeedButton1Click(Sender: TObject);
 Var
- ap, tm, td, ak : Real;
- dokl : Integer;
- pom : String;
+  ap, tm, td, ak: Real;
+  dokl: Integer;
+  pom: String;
 begin
-if (Edit1.Text<>'') and (Edit2.Text<>'') and (Edit3.Text<>'') then
-Begin
- pom:=Edit1.Text;
-if Pos('.',pom)>0 then
- Begin
-  pom:=StringReplace(pom,'.',',',[rfReplaceAll, rfIgnoreCase]);
-  Edit1.Text:=pom;
- End;
- pom:=Edit2.Text;
-if Pos('.',pom)>0 then
- Begin
-  pom:=StringReplace(pom,'.',',',[rfReplaceAll, rfIgnoreCase]);
-  Edit2.Text:=pom;
- End;
-pom:=Edit3.Text;
-if Pos('.',pom)>0 then
- Begin
-  pom:=StringReplace(pom,'.',',',[rfReplaceAll, rfIgnoreCase]);
-  Edit3.Text:=pom;
- End;
+  if (Edit1.Text <> '') and (Edit2.Text <> '') and (Edit3.Text <> '') then
+  Begin
+    pom := Edit1.Text;
+    if Pos('.', pom) > 0 then
+    Begin
+      pom := StringReplace(pom, '.', ',', [rfReplaceAll, rfIgnoreCase]);
+      Edit1.Text := pom;
+    End;
+    pom := Edit2.Text;
+    if Pos('.', pom) > 0 then
+    Begin
+      pom := StringReplace(pom, '.', ',', [rfReplaceAll, rfIgnoreCase]);
+      Edit2.Text := pom;
+    End;
+    pom := Edit3.Text;
+    if Pos('.', pom) > 0 then
+    Begin
+      pom := StringReplace(pom, '.', ',', [rfReplaceAll, rfIgnoreCase]);
+      Edit3.Text := pom;
+    End;
 
- dokl:=SpinEdit1.Value;
- ap:=StrToFloat(Edit1.Text);
- tm:=StrToFloat(Edit2.Text);
- td:=StrToFloat(Edit3.Text);
+    dokl := SpinEdit1.Value;
+    ap := StrToFloat(Edit1.Text);
+    tm := StrToFloat(Edit2.Text);
+    td := StrToFloat(Edit3.Text);
 
- if td<>0 then
- Begin
- if CheckBox1.Checked=True then ak:=ap*(exp((-0.693*tm)/td))
- else ak:=(ap)/(power(2,tm/td));
- End
- else ak:=0;
+    if td <> 0 then
+    Begin
+      if cbox_algorythm.Checked = True then
+        ak := ap * (exp((-0.693 * tm) / td))
+      else
+        ak := (ap) / (power(2, tm / td));
+    End
+    else
+      ak := 0;
 
- Edit4.Text:=FloatToStr(RoundTo(ak,-dokl));
-End;
+    Edit4.Text := FloatToStr(RoundTo(ak, -dokl));
+  End;
 end;
 
-procedure TAOknoGl.SpeedButton2Click(Sender: TObject);
+procedure TAOknoGl.btn_change_isotopeClick(Sender: TObject);
 Var
- aktywnosc, okres : String;
+  aktywnosc, okres: String;
 begin
- aktywnosc:=ADODataSet1.FieldByName('Aktywnosc').AsString;
- okres:=ADODataSet1.FieldByName('Polrozpad').AsString;
- Edit1.Text:=aktywnosc;
- Edit3.Text:=okres;
- Edit4.Text:='';
+  aktywnosc := ADODataSet1.FieldByName('Aktywnosc').AsString;
+  okres := ADODataSet1.FieldByName('Polrozpad').AsString;
+  Edit1.Text := aktywnosc;
+  Edit3.Text := okres;
+  Edit4.Text := '';
 
- Label13.Visible:=True;
- Label14.Visible:=True;
- Label15.Visible:=True;
- Label16.Visible:=True;
- SpeedButton3.Visible:=True;
- Edit5.Visible:=True;
- SpinEdit2.Visible:=True;
+  Label13.Visible := True;
+  Label14.Visible := True;
+  Label15.Visible := True;
+  Label16.Visible := True;
+  btn_create_chart.Visible := True;
+  Edit5.Visible := True;
+  SpinEdit2.Visible := True;
 end;
 
-procedure TAOknoGl.SpeedButton3Click(Sender: TObject);
+procedure TAOknoGl.btn_create_chartClick(Sender: TObject);
 Var
- i, ile : Integer;
- ap, tm, td, ak, skok : Real;
+  i, ile: Integer;
+  ap, tm, td, ak, skok: Real;
 begin
-Chart1.Series[0].Clear;
-Edit2.Text:='0';
-ile:=SpinEdit2.Value;
-skok:=StrToFloat(Edit5.Text);
+  Chart1.Series[0].Clear;
+  Edit2.Text := '0';
+  ile := SpinEdit2.Value;
+  skok := StrToFloat(Edit5.Text);
 
-for i := 1 to ile do
- Begin
-  ap:=StrToFloat(Edit1.Text);
-  tm:=StrToFloat(Edit2.Text);
-  td:=StrToFloat(Edit3.Text);
+  for i := 1 to ile do
+  Begin
+    ap := StrToFloat(Edit1.Text);
+    tm := StrToFloat(Edit2.Text);
+    td := StrToFloat(Edit3.Text);
 
- if CheckBox1.Checked=True then ak:=ap*(exp((-0.693*tm)/td))
- else ak:=(ap)/(power(2,tm/td));
- tm:=tm+skok;
- if ak<0 then ak:=0;
- Edit2.Text:=FloatToSTr(tm);
-  Chart1.Series[0].Add(ak,'+'+IntToStr(i),536870912);
- End;
-Chart1.Series[0].Add(0,'+...',536870912);
-TabSheet3.TabVisible:=True;
-TabSheet3.Show;
+    if cbox_algorythm.Checked = True then
+      ak := ap * (exp((-0.693 * tm) / td))
+    else
+      ak := (ap) / (power(2, tm / td));
+    tm := tm + skok;
+    if ak < 0 then
+      ak := 0;
+    Edit2.Text := FloatToStr(tm);
+    Chart1.Series[0].Add(ak, '+' + IntToStr(i), 536870912);
+  End;
+  Chart1.Series[0].Add(0, '+...', 536870912);
+  TabSheet3.TabVisible := True;
+  TabSheet3.Show;
 end;
 
 procedure TAOknoGl.SpeedButton4Click(Sender: TObject);
 Var
- nap, wartS : String;
- wart : Real;
- dokl : Integer;
+  nap, wartS: String;
+  wart: Real;
+  dokl: Integer;
 begin
   wartS := Edit1.Text;
   if wartS <> '' then
   Begin
-    wart:=StrToFloat(wartS);
+    wart := StrToFloat(wartS);
     dokl := SpinEdit1.Value;
     if Label10.Caption = 'MBq' then
       nap := 'GBq';
